8b14cd8b55ae7183f39afe38d962c5ce405069f6
Evangelos <ec00727@surrey.ac.uk>
2019-02-13 19:11:54 +0000

    Migrated completely to AllAuth
    Customized the e-mail messages. Removed redundant templates, urls and views offered by the Users app in the past. Kept forms since they might be used to override the AllAuth ones later on

--- users/urls.py
+++ users/urls.py
@@ -8,10 +8,5 @@ except ImportError:
 app_name = 'users'
 
 urlpatterns = [
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', views.user_login, name='login'),
-    url(r'^activate/$', views.activate, name='activate'),
-    url(r'^logout$', views.user_logout, name='logout'),
-    url(r'^reset/$', views.pass_reset, name='reset'),
 ]
 

251c26058191a270b411719aada72aa321cd581e
evangelos-ch <angelos.h@hotmail.com>
2019-12-30 17:57:25 +0200

    Test User models

---
+++ users/tests/__init__.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+
+import pytest
+
+from MangAdventure.tests import MangadvTestBase
+
+from reader.models import Series
+
+
+@pytest.mark.usefixtures("django_db_setup")
+class UsersTestBase(MangadvTestBase):
+    def setup_method(self):
+        super().setup_method()
+        self.series = Series.objects.get(pk=1)
+        self.user = User.objects.get(pk=1)
---
+++ users/tests/test_models.py
@@ -0,0 +1,52 @@
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+
+from pytest import raises
+
+from reader.models import Series
+from users.models import Bookmark, UserProfile, _avatar_uploader
+
+from . import UsersTestBase
+
+
+class TestBookmark(UsersTestBase):
+    def test_create(self):
+        """Test object creation & relations."""
+        bookmark = Bookmark.objects.create(user=self.user, series=self.series)
+        assert bookmark.series == self.series
+        assert bookmark.user == self.user
+        assert len(self.user.bookmarks.all()) == 1
+        assert self.user.bookmarks.get(series=self.series)
+
+    def test_integrity(self):
+        """Test that series and user are unique together,
+         but not individually."""
+        Bookmark.objects.create(user=self.user, series=self.series)
+        user2 = User.objects.get(pk=2)
+        series2 = Series.objects.get(pk=2)
+        assert Bookmark.objects.create(user=user2, series=self.series)
+        assert Bookmark.objects.create(user=self.user, series=series2)
+
+        with raises(IntegrityError):
+            Bookmark.objects.create(user=self.user, series=self.series)
+
+
+class TestUserProfile(UsersTestBase):
+    def setup_method(self):
+        super().setup_method()
+        self.profile = UserProfile.objects.create(user=self.user, bio="Test")
+
+    def test_create(self):
+        """Test object creation & relations."""
+        assert self.profile.bio == "Test"
+        assert self.profile.user == self.user
+        assert self.user.profile == self.profile
+
+        assert str(self.profile) == str(self.user)
+
+
+class TestUtils(UsersTestBase):
+    def test_avatar_uploader(self):
+        profile = UserProfile.objects.create(user=self.user)
+        upload_dir = _avatar_uploader(profile, "whatever.png")
+        assert upload_dir == f"users/{profile.pk}/avatar.png"

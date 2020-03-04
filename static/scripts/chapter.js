02a409c4abed06e225e06f152d0a18a33b83e716
ObserverOfTime <chronobserver@disroot.org>
2020-03-04 17:37:53 +0200

    Implement bidirectional page clicks

--- static/scripts/chapter.js
+++ static/scripts/chapter.js
@@ -1,42 +1,37 @@
-(function(img, ph) {
-  function changePage(change) {
-    const url = img.getAttribute(`data-${change}`);
-    if(url) return url;
-    const diff = (change === 'next') ? 1 : -1;
-    return location.pathname.replace(
-      /(\d+)\/$/, (m, n) => `${Number(n) + diff}/`
-    );
-  }
+(function(ph) {
+  const changePage = (rel) =>
+    document.querySelector(`.control[rel="${rel}"]`).href;
 
-  img.src = ph.getAttribute('data-img');
-  img.alt = `Page ${ph.getAttribute('data-num')}`;
-  img.className = 'chapter-page';
-  img.setAttribute('data-next', ph.getAttribute('data-next') || '');
-  img.setAttribute('data-prev', ph.getAttribute('data-prev') || '');
-  img.addEventListener('click', () => {
-    location.pathname = changePage('next');
+  const img = ph.nextElementSibling;
+
+  if(img.complete) ph.remove();
+  else img.addEventListener('load', () => ph.remove(), true);
+
+  img.addEventListener('click', function(evt) {
+    const width = this.offsetWidth;
+    const coord = evt.clientX - this.getBoundingClientRect().left;
+    location.href = changePage(coord < width / 2 ? 'prev' : 'next');
   });
-  img.addEventListener('load', () => {
-    const dd = document.getElementById('dropdowns');
-    const ct = document.getElementById('controls');
-    ph.parentNode.replaceChild(img, ph);
-    dd.removeAttribute('class');
-    ct.removeAttribute('class');
-    const ci = document.querySelector('.curr-page input');
-    ci.addEventListener('keyup', evt => {
-      if(evt.keyCode !== 13) return;
-      const n = Number(ci.value);
-      if(!n || !Number.isInteger(n)) return;
-      if(n === Number(ci.placeholder)) return;
-      if(n > Number(ci.getAttribute('data-max'))) return;
-      location.pathname = location.pathname.replace(/\d+\/$/, `${n}/`);
-    });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('dropdowns').removeAttribute('class');
+    document.getElementById('controls').removeAttribute('class');
+    document.querySelector('.curr-page input')
+      .addEventListener('keyup', function(evt) {
+        if(evt.keyCode !== 13) return;
+        const n = Number(this.value);
+        if(!n || !Number.isInteger(n)) return;
+        if(n === Number(this.placeholder)) return;
+        if(n > Number(this.dataset.max)) return;
+        if(n > 0) location.href = `../${n}/`;
+      });
   });
+
   document.body.addEventListener('keyup', evt => {
     switch(evt.keyCode) {
-      case 37: location.pathname = changePage('prev'); break;
-      case 39: location.pathname = changePage('next'); break;
+      case 37: location.href = changePage('prev'); break;
+      case 39: location.href = changePage('next'); break;
       default: return;
     }
   });
-})(new Image(), document.getElementById('placeholder'));
+})(document.getElementById('placeholder'));

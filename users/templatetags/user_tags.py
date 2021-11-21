b570f1aa45475630fbee9578d8d24c9c2ba2fed5
ObserverOfTime <chronobserver@disroot.org>
2021-11-21 17:12:04 +0200

    Introduce get_oauth_providers template tag
    It automatically filters out unavailable providers

--- users/templatetags/user_tags.py
+++ users/templatetags/user_tags.py
@@ -1,26 +1,27 @@
 """Template tags of the users app."""
 
-from operator import attrgetter
-from typing import Iterable, List
+from typing import List
 
 from django.template.defaultfilters import register
 
 from allauth.socialaccount.models import SocialApp
+from allauth.socialaccount.providers import registry
+from allauth.socialaccount.templatetags.socialaccount import (
+    provider_login_url as _provider_login_url
+)
 
 
-@register.filter
-def available(providers: Iterable[SocialApp]) -> List[SocialApp]:
-    """
-    Filter out the unavailable OAuth providers.
-
-    :param providers: The original list of OAuth providers.
-
-    :return: A list of available OAuth providers.
-    """
-    ids = list(map(attrgetter('id'), providers))
-    objects = SocialApp.objects \
-        .filter(provider__in=ids).values_list('provider', flat=True)
-    return list(filter(lambda p: p.id in objects, providers))
+@register.simple_tag
+def get_oauth_providers() -> List:
+    """Get a list of available OAuth providers."""
+    registry.load()
+    return [
+        registry.provider_map[p](None) for p in
+        SocialApp.objects.values_list('provider', flat=True)
+    ]
 
 
-__all__ = ['available']
+# :func:`allauth.socialaccount.templatetags.socialaccount.provider_login_url`
+provider_login_url = register.tag(_provider_login_url)
+
+__all__ = ['get_oauth_providers', 'provider_login_url']

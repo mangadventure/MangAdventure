229be1fa2f441fe415b8d3eeb56bf7675bb95d58
ObserverOfTime <chronobserver@disroot.org>
2023-12-14 12:33:05 +0200

    Remove API v1 views

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,10 +1,9 @@
 """The URLconf of the api.v1 app."""
 
+from django.http import HttpResponseGone
 from django.urls import path, register_converter
 
 from MangAdventure.converters import FloatConverter
-
-from . import views
 
 register_converter(FloatConverter, 'float')
 
@@ -17,20 +16,23 @@ _groups = 'groups'
 #: The URL namespace of the api.v1 app.
 app_name = 'v1'
 
+def _gone(*args, **kwargs) -> HttpResponseGone:
+    return HttpResponseGone('Use API v2 instead.')
+
 #: The URL patterns of the api.v1 app.
 urlpatterns = [
-    path('releases/', views.all_releases, name='releases'),
-    path('series/', views.all_series, name='all_series'),
-    path(f'{_series}/', views.series, name='series'),
-    path(f'{_volume}/', views.volume, name='volume'),
-    path(f'{_volume}/<float:num>/', views.chapter, name='chapter'),
-    path(f'{_authors}/', views.all_people, name='all_authors'),
-    path(f'{_authors}/<int:p_id>/', views.person, name='author'),
-    path(f'{_artists}/', views.all_people, name='all_artists'),
-    path(f'{_artists}/<int:p_id>/', views.person, name='artist'),
-    path(f'{_groups}/', views.all_groups, name='all_groups'),
-    path(f'{_groups}/<int:g_id>/', views.group, name='group'),
-    path('categories/', views.categories, name='categories'),
+    path('releases/', _gone, name='releases'),
+    path('series/', _gone, name='all_series'),
+    path(f'{_series}/', _gone, name='series'),
+    path(f'{_volume}/', _gone, name='volume'),
+    path(f'{_volume}/<float:num>/', _gone, name='chapter'),
+    path(f'{_authors}/', _gone, name='all_authors'),
+    path(f'{_authors}/<int:p_id>/', _gone, name='author'),
+    path(f'{_artists}/', _gone, name='all_artists'),
+    path(f'{_artists}/<int:p_id>/', _gone, name='artist'),
+    path(f'{_groups}/', _gone, name='all_groups'),
+    path(f'{_groups}/<int:g_id>/', _gone, name='group'),
+    path('categories/', _gone, name='categories'),
 ]
 
 __all__ = ['app_name', 'urlpatterns']
--- docs/api/includes/status.rst
+++
@@ -1,7 +0,0 @@
-Status Codes
-~~~~~~~~~~~~
-
-* `200 OK <https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200>`_
-  - No error.
-* `304 Not Modified <https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/304>`_
-  - The request was conditional and the content hasn't been modified.
